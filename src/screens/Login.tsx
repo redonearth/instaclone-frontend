3818d02543de792e0685d00f14cb22b7f25d6ea2
Jisang Hong <wona23@gmail.com>
2022-07-19 17:34:18 +0900

    로그인 UI 스타일링

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,19 +1,131 @@
+import {
+  faFacebookSquare,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
-import { darkModeVar, isLoggedInVar } from '../apollo';
+// import { darkModeVar, isLoggedInVar } from '../apollo';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.fontColor};
+const WhiteBox = styled.div`
+  background-color: white;
+  border: 1px solid rgb(219, 219, 219);
+  width: 100%;
+`;
+
+const TopBox = styled(WhiteBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+  form {
+    margin-top: 35px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    input {
+      width: 100%;
+      border-radius: 3px;
+      padding: 7px;
+      background-color: #fafafa;
+      border: 0.5px solid rgb(219, 219, 219);
+      margin-top: 5px;
+      box-sizing: border-box;
+      &::placeholder {
+        font-size: 12px;
+      }
+      &:last-child {
+        border: none;
+        margin-top: 12px;
+        background-color: #0095f6;
+        color: white;
+        text-align: center;
+        padding: 8px 0px;
+        font-weight: 700;
+      }
+    }
+  }
+`;
+
+const BottomBox = styled(WhiteBox)`
+  padding: 20px 0px;
+  text-align: center;
+  a {
+    font-weight: 700;
+    color: #0095f6;
+  }
+`;
+
+const Wrapper = styled.div`
+  max-width: 350px;
+  width: 100%;
+`;
+
+const Separator = styled.div`
+  margin: 20px 0px 30px 0px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  div {
+    width: 100%;
+    height: 2px;
+    background-color: rgb(219, 219, 219);
+  }
+  span {
+    margin: 0px 10px;
+    color: rgb(219, 219, 219);
+  }
+`;
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 5px;
+    font-weight: 700;
+  }
 `;
 
 function Login() {
   return (
     <Container>
-      <Title>로그인</Title>
-      <button onClick={() => isLoggedInVar(true)}>지금 로그인!</button>
-      <button onClick={() => darkModeVar(true)}>다크 모드</button>
-      <button onClick={() => darkModeVar(false)}>라이트 모드</button>
+      <Wrapper>
+        <TopBox>
+          <div>
+            <FontAwesomeIcon icon={faInstagram} size={'3x'} />
+          </div>
+          <h1>Instagram</h1>
+          <form>
+            <input type="text" placeholder="사용자 이름" />
+            <input type="text" placeholder="비밀번호" />
+            <input type="submit" placeholder="로그인" />
+          </form>
+          <Separator>
+            <div />
+            <span>또는</span>
+            <div />
+          </Separator>
+          <FacebookLogin>
+            <FontAwesomeIcon icon={faFacebookSquare} />
+            <span>Facebook으로 로그인</span>
+          </FacebookLogin>
+          <span>비밀번호를 잊으셨나요?</span>
+        </TopBox>
+        <BottomBox>
+          <span>계정이 없으신가요?</span> <a href="#">가입하기</a>
+        </BottomBox>
+      </Wrapper>
     </Container>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -2,18 +2,29 @@ import { createGlobalStyle, DefaultTheme } from 'styled-components';
 import reset from 'styled-reset';
 
 export const lightTheme: DefaultTheme = {
-  bgColor: 'lightgray',
+  bgColor: '#fafafa',
   fontColor: '#2c2c2c',
 };
 
 export const darkTheme: DefaultTheme = {
   bgColor: '#2c2c2c',
-  fontColor: 'lightgray',
+  fontColor: '#fafafa',
 };
 
 export const GlobalStyles: any = createGlobalStyle`
   ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  input {
+    all: unset;
+  }
   body {
     background-color: ${(props) => props.theme.bgColor};
+    font-size: 14px;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  a {
+    text-decoration: none;
   }
 `;
