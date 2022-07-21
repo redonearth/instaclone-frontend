6c72fdd6c9a1559842ab81cc3f7ba64d901af84d
Jisang Hong <wona23@gmail.com>
2022-07-21 11:00:33 +0900

    Apollo Client 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useReactiveVar } from '@apollo/client';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { darkModeVar, isLoggedInVar } from './apollo';
+import { client, darkModeVar, isLoggedInVar } from './apollo';
 import routes from './routes';
 import Home from './screens/Home';
 import Login from './screens/Login';
@@ -13,17 +13,21 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Routes>
-            <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-            {!isLoggedIn && <Route path={routes.signup} element={<Signup />} />}
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Routes>
+              <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+              {!isLoggedIn && (
+                <Route path={routes.signup} element={<Signup />} />
+              )}
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
