949706d2b8bfd78a08ed5e340b16fe2ceb04e42f
Jisang Hong <wona23@gmail.com>
2022-08-02 16:18:20 +0900

    페이지 제목 추가

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import PageTitle from '../components/PageTitle';
 import Photo from '../components/feed/Photo';
 
 interface ISeeFeedQuery {
@@ -41,6 +42,7 @@ function Home() {
   const { data } = useQuery<ISeeFeedQuery>(SEE_FEED_QUERY);
   return (
     <>
+      <PageTitle title="피드" />
       {data?.seeFeed?.map((photo) => (
         <Photo key={photo.id} {...photo} />
       ))}
